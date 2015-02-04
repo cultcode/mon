0a36eb2cf313ea8eec879a8d24396319f767109b
Rovere <rovluo@gmail.com>
2015-02-04 13:39:37 +0800

    remove Base64Convert.c Base64Convert.h

--- Base64Convert.h
+++
@@ -1,8 +0,0 @@
-#ifndef BASE64CONVERT_H
-#define BASE64CONVERT_H
-
-extern int Base64Encode(const char* message, char* buffer, int length);
-
-extern int Base64Decode(char* b64message, char* buffer, int length);
-
-#endif
