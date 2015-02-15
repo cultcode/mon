fa2ee65c6f7288b15f9b6066b452bbc3bdddebcf
Rovere <rovluo@gmail.com>
2015-02-15 23:41:03 +0800

    fix NodeStatusSvr.sh

--- NodeStatusSvr.sh
+++ NodeStatusSvr.sh
@@ -7,7 +7,7 @@ workdir=$(dirname "$0")
 process=( 'NodeStatusSvr' )
 
 for p in ${process[@]}; do
-	item=`ps -ef | grep "$p$" | grep -v "grep"`
+	item=`ps -ef | grep -v "$0" | grep "$p" | grep -v "grep"`
 	if [ "${#item}" -gt 0 ];then
 		echo ""		 
 	else	
