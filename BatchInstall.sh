6a7a6513d1be35f5a104fde86e037ad35ca29c35
Rovere <rovluo@gmail.com>
2015-02-27 06:33:11 +0000

    run local expect

--- BatchInstall.sh
+++ BatchInstall.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 usage () {
-  echo "Usage: $0 USER_PASSWORD ROOT_PASSWORD"
+  echo "Usage: $0 IP_FILE USER USER_PASSWORD ROOT_PASSWORD"
 }
 
-if [ "$#" -ne 2 ];then
+if [ "$#" -ne 4 ];then
   usage;
   exit 1;
 fi;
 
-password_user=$1
-password_su=$2
+file_ip=$1
+user=$2
+password_user=$3
+password_su=$4
 
-for ip in $(cat ./ip.txt | sed '/^\s*\$/d')
+for ip in $(cat $file_ip | sed '/^\s*\$/d')
+expect -c "
+  set timeout -1
+  spawn scp /usr/bin/expect  $user@$ip:
+  expect *assword:
+  send $password_user\r
+  expect eof
+  spawn scp /lib64/libexpect.so $user@$ip:
+  expect *assword:
+  send $password_user\r
+  expect eof
+"
+
 do
   CMD="\
 cd && \
+export PATH=\$PATH:. && \
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:. && \
 rm -rf ./NodeStatusSvr.install && \
 wget -nH -P . -N --no-check-certificate http://github.com/cultcode/mon/raw/master/NodeStatusSvr.install && \
 chmod +x ./NodeStatusSvr.install && \
@@ -23,7 +39,7 @@ chmod +x ./NodeStatusSvr.install && \
 echo $CMD
   expect -c "
     set timeout -1
-    spawn ssh -o StrictHostKeyChecking=no luowei@$ip \"$CMD\"
+    spawn ssh -o StrictHostKeyChecking=no $user@$ip \"$CMD\"
     expect *assword:
     send $password_user\r
     expect eof
